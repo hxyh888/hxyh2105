a7ac21f112848ce9b8aaeb68b4ef46375c8c538c
hxyh <hxyhsoft@163.com>
2021-09-14 09:56:33 +0800

    add te_test.py

---
+++ re_test.py
@@ -0,0 +1,9 @@
+import re
+
+name_list = ["name","!admin","name?","de#mo","1name","na1n","_name"]
+for i in name_list:
+    result = re.match("^[A-Za-z_][A-Za-z_0-9]{%d}[A-Za-z_0-9]$"%(len(i)-2),i)
+    if result:	
+        print("%sfuheguize"%result.group())
+    else:
+        print("%sbufuheguize"%i)
